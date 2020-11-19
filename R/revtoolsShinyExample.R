07bbd0ae86773a3b51608e1c6c88a7ad3f5340cc
lizlaw <grasshoppertee@gmail.com>
2020-11-19 16:21:16 +0100

    revtools example (fix to always use data)

--- R/revtoolsShinyExample.R
+++ R/revtoolsShinyExample.R
@@ -8,8 +8,8 @@ data <- revtools::read_bibliography(file_names)
 
 data_unique <- screen_duplicates(data)
 
-data_ti_screened <- screen_titles(data_unique %>% sample_n(10))
+data_ti_screened <- screen_titles(data %>% sample_n(10))
 
-data_ab_screened <- screen_abstracts(data_ti_screened %>% sample_n(5))
+data_ab_screened <- screen_abstracts(data %>% sample_n(5))
 
-data_topic_screened <- screen_topics(data_unique %>% sample_n(100))
+data_topic_screened <- screen_topics(data %>% sample_n(100))
